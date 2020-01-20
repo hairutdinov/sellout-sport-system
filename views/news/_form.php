2567ebaa3b49de9a988942d7aa4e79adeb386fdb
Bulat Khayrutdinov <bulatxajrutdinoff@yandex.ru>
2020-01-20 20:03:19 +0300

    Добавил классы Bootstrap4 для формы

--- views/news/_form.php
+++ views/news/_form.php
@@ -12,21 +12,25 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+    <div class="row justify-content-sm-center">
+      <div class="col-sm-10"><?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?></div>
 
-    <?= $form->field($model, 'author')->textInput(['maxlength' => true]) ?>
+      <div class="col-sm-10"><?= $form->field($model, 'author')->textInput(['maxlength' => true]) ?></div>
 
-    <?= $form->field($model, 'url')->textarea(['rows' => 6]) ?>
+      <div class="col-sm-10"><?= $form->field($model, 'url')->textarea(['rows' => 6]) ?></div>
 
-    <?= $form->field($model, 'urlToImage')->textarea(['rows' => 6]) ?>
+      <div class="col-sm-10"><?= $form->field($model, 'urlToImage')->textarea(['rows' => 6]) ?></div>
 
-    <?= $form->field($model, 'publishedAt')->textInput() ?>
+      <div class="col-sm-10"><?= $form->field($model, 'publishedAt')->textInput() ?></div>
 
-    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+      <div class="col-sm-10"><?= $form->field($model, 'content')->textarea(['rows' => 6]) ?></div>
 
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+      <div class="form-group col-sm-10">
+          <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+      </div>
     </div>
+
+
 
     <?php ActiveForm::end(); ?>
 
